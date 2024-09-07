ebf5c745e873fc3b7f1d886c3c16cc8a5b025db8
Origami Official <64251776+origamiofficial@users.noreply.github.com>
2024-09-07 15:00:44 +0930

    Release of v5

--- Windows Bash Scripts/Windows_Health_Restore.cmd
+++ Windows Bash Scripts/Windows_Health_Restore.cmd
@@ -1,5 +1,5 @@
 @echo off  
-title Windows Health Restore V4
+title Windows Health Restore v5
 
 net session>nul 2>&1
 if %errorlevel%==0 goto main
@@ -16,18 +16,28 @@ echo ***                                       by R Lab inc.        ***
 echo ******************************************************************
 echo.
 echo.
-echo "##### Step 1: Scanning Now #####"
-SFC /scannow
-echo "##### Step 2: Checking Health #####"
-DISM /Online /Cleanup-Image /CheckHealth
-echo "##### Step 3: Scanning Health #####"
-DISM /Online /Cleanup-Image /ScanHealth
-echo "##### Step 4: Restoring Health #####"
-DISM /Online /Cleanup-Image /RestoreHealth
-echo "##### Step 5: Flushing DNS Cache #####"
+echo "##### Step 01/11: Checking System Files #####"
+sfc /scannow
+echo "##### Step 02/11: Checking System Image Health #####"
+dism /Online /Cleanup-Image /CheckHealth
+echo "##### Step 03/11: Scanning System Image Health #####"
+dism /Online /Cleanup-Image /ScanHealth
+echo "##### Step 04/11: Restoring System Image Health #####"
+dism /Online /Cleanup-Image /RestoreHealth
+echo "##### Step 05/11: Analyzing Component Store #####"
+dism /online /cleanup-image /analyzecomponentstore
+echo "##### Step 06/11: Clearing DNS Cache #####"
 ipconfig /flushdns
+echo "##### Step 07/11: Disabling Processor Idle Disable #####"
+PowerCfg /SETACVALUEINDEX SCHEME_CURRENT SUB_PROCESSOR IDLEDISABLE 000
+echo "##### Step 08/11: Activating Current Power Plan #####"
+PowerCfg /SETACTIVE SCHEME_CURRENT
+echo "##### Step 09/11: Deleting Minidump Files #####"
+del /f /s /q %systemroot%\minidump\*.*
+echo "##### Step 10/11: Deleting Dump Files #####"
+del *.dmp /s
+echo "##### Step 11/11: Restarting Your Computer #####"
 echo "##### We're done now, run again within a month #####"
-echo "##### Your computer will RESTART now #####"
-PAUSE
+pause
 shutdown /r /t 0
 exit
